8b18ae43511b42773963393d70911e677325c6af
VolvoRider <kfirswit@gmail.com>
2022-11-25 15:50:09 +0200

    added pdp 2

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,7 @@ import { Card, createTheme, Stack, Grid, ThemeProvider, Typography } from '@mui/
 import { Container, keyframes } from '@mui/system';
 import React from 'react'
 import IQueryIcon from "../assets/iquery_icon.png"
+import PDPIcon from "../assets/pdp_icon.png"
 import zero from '../assets/iquery/0.png'
 import first from '../assets/iquery/1.png'
 import second from '../assets/iquery/2.png'
@@ -10,10 +11,18 @@ import four from '../assets/iquery/4.png'
 import fifth from '../assets/iquery/5.png'
 import sixth from '../assets/iquery/6.png'
 
+import firstPDP from '../assets/pdp/1.png'
+import secondPDP from '../assets/pdp/2.png'
+import thirdPDP from '../assets/pdp/3.png'
+import forthPDP from '../assets/pdp/4.png'
+import fifthPDP from '../assets/pdp/5.png'
+import sixthPDP from '../assets/pdp/6.png'
+
 import Carousel from 'react-material-ui-carousel'
 
 
 const slideImages = [zero, first, second, third, four, fifth, sixth];
+const slideImagesPDP = [firstPDP, secondPDP, thirdPDP, forthPDP, fifthPDP, sixthPDP];
 
 const myTheme = createTheme({
     typography: {
@@ -56,7 +65,7 @@ export const Projects = () => {
 
                             <Grid item xs={8} md={6}>
                                 <Card sx={{padding: 2, backgroundColor: 'transparent', shadowOpacity: 0, ":hover": {boxShadow: 20}}}>
-                                    <Typography mb={2} variant='h4' color="common.white">IQuery (To Be Released) </Typography>
+                                    <Typography mb={2} variant='h4' color="common.white">IQuery</Typography>
                                     <Stack alignItems={"center"} direction="row">
                                         <Typography variant="p" color="common.white">
                                             IQuery is a tool that helps you find connections between any two questions that YOU choose. It provides the percentage of the relation, and presents statistics with graphs.
@@ -74,6 +83,44 @@ export const Projects = () => {
                                 <Carousel sx={{width: "80%", m:2}} >
                                     {
                                         slideImages.map((image) =>{
+                                            return <Card display="flex"><img src={image} alt="slide" style={{width: "100%", height: "100%"}}/></Card>
+                                        } )
+                                    }
+                                </Carousel>
+                            </Grid>
+
+                        </Grid>
+
+                    </Container>
+
+                    <Container id="pdp2">
+                        <Grid container spacing={3} alignItems='center' justifyContent='center'>
+                            <Grid item xs={4} md={2}>
+                                <Card display={"flex"} sx={{display: {width: "60%", height: "60%"}, mr: 2, boxShadow:20, animation: `${bounceEffect} 2s infinite ease`}}>
+                                    <img style={{width: "100%", height: "100%"}} src={PDPIcon} alt="pdpicon" />
+                                </Card>
+                            </Grid>
+
+                            <Grid item xs={8} md={6}>
+                                <Card sx={{padding: 2, backgroundColor: 'transparent', shadowOpacity: 0, ":hover": {boxShadow: 20}}}>
+                                    <Typography mb={2} variant='h4' color="common.white">Pocket Day Planner 2</Typography>
+                                    <Stack alignItems={"center"} direction="row">
+                                        <Typography variant="p" color="common.white">
+                                            Pocket Day Planner 2 is an app for simply planning your day, managing and sharing tasks easily, using modern design.
+                                        </Typography>
+                                    </Stack>
+                                    <Stack alignItems={{xs: "center", md: "end"}}>
+                                        <a style={{display: "flex", width: 200}} href='https://play.google.com/store/apps/details?id=com.mooncrest.pocketdayplanner'>
+                                            <img  alt='Get it on Google Play' style={{display: "flex", width: 200}} src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/>
+                                        </a>
+                                    </Stack>
+                                </Card>
+                            </Grid>
+
+                            <Grid item xs={6} md={4}>
+                                <Carousel sx={{width: "80%", m:2}} >
+                                    {
+                                        slideImagesPDP.map((image) =>{
                                             return <Card display="flex"><img src={image} alt="slide" style={{width: "100%", height: "100%"}}/></Card>
                                         } )
                                     }
